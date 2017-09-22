5d50417b18e3e28d3c5a1958e5de40fa797e368d
yu12mang <514543120@qq.com>
2017-09-22 18:55:36 +0800

    first commit

---
+++ cutvideolib/src/main/java/com/video/trimmer/natives/VideoTrimmer.java
@@ -0,0 +1,14 @@
+package com.video.trimmer.natives;
+
+public class VideoTrimmer {
+	public static native int trim(String inputFile, String outputFile, int start, int duration);
+
+	public int trim_(String inputFileName, String outFileName, int start, int duration) {
+		return trim(inputFileName, outFileName, start, duration);
+	}
+
+	static {
+		System.loadLibrary("ffmpeg");
+		System.loadLibrary("vtrimmer");
+	}
+}
